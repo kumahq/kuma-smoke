592c679d17595031cacec48269d84ade7a642e48
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2024-12-25 11:25:03 +0800

    feat(cmd): do not install kuma as an addon when deploying kubernetes cluster
    Signed-off-by: Jay Chen <1180092+jijiechen@users.noreply.github.com>

---
+++ pkg/smoke/k8s-versions.go
@@ -0,0 +1,6 @@
+package smoke
+
+var (
+	MinSupportedKubernetesVer = "1.25.16" // referenced from https://github.com/kumahq/kuma/blob/2.9.2/mk/dev.mk#L24-L25
+	MaxSupportedKubernetesVer = "1.31.1"
+)
