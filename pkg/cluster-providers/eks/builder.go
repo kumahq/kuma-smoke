fc8732cbd1cffbcef31b1038d07a2283bd0673f3
Jay Chen <1180092+jijiechen@users.noreply.github.com>
2025-01-09 17:28:54 +0800

    feat(cmd): fix subnet ids when creating EKS cluster
    Signed-off-by: Jay Chen <1180092+jijiechen@users.noreply.github.com>

--- pkg/cluster-providers/eks/builder.go
+++ pkg/cluster-providers/eks/builder.go
@@ -9,6 +9,7 @@ import (
 	ec2Types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/aws/aws-sdk-go-v2/service/eks"
 	"github.com/aws/aws-sdk-go-v2/service/eks/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/blang/semver/v4"
 	"github.com/google/uuid"
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
@@ -75,21 +76,23 @@ func (b *Builder) Build(ctx context.Context) (clusters.Cluster, error) {
 		return nil, errors.Wrap(err, "failed to load AWS SDK config")
 	}
 
-	creds, err := cfg.Credentials.Retrieve(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to load AWS identity")
-	}
-
+	stsClient := sts.NewFromConfig(cfg)
 	ec2Client := ec2.NewFromConfig(cfg)
 	eksClient := eks.NewFromConfig(cfg)
 
+	callerIdentity, err := stsClient.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get caller identity")
+	}
+
+	accountId := *callerIdentity.Account
 	_, subnetIDs, err := createVPC(ctx, ec2Client, cfg.Region)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create VPC")
 	}
 
 	clusterName := b.Name
-	clusterRoleArn := fmt.Sprintf("arn:aws:iam::%s:role/EKSClusterRole", creds.AccountID)
+	clusterRoleArn := fmt.Sprintf("arn:aws:iam::%s:role/EKSClusterRole", accountId)
 
 	eksCreateInput := &eks.CreateClusterInput{
 		Name:    &clusterName,
@@ -109,7 +112,7 @@ func (b *Builder) Build(ctx context.Context) (clusters.Cluster, error) {
 		return nil, errors.Wrapf(err, "failed while waiting for EKS cluster %s to become active", clusterName)
 	}
 
-	nodeRoleArn := fmt.Sprintf("arn:aws:iam::%s:role/EKSNodeRole", creds.AccountID)
+	nodeRoleArn := fmt.Sprintf("arn:aws:iam::%s:role/EKSNodeRole", accountId)
 	err = createNodeGroup(ctx, eksClient, clusterName, nodeRoleArn, b.nodeMachineType, subnetIDs)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to create EKS node group for cluster %s", clusterName)
@@ -127,12 +130,17 @@ func createVPC(ctx context.Context, client *ec2.Client, region string) (string, 
 	}
 
 	vpcID := *vpcOutput.Vpc.VpcId
-	// Enable DNS support for the VPC
 	_, err = client.ModifyVpcAttribute(context.TODO(), &ec2.ModifyVpcAttributeInput{
 		VpcId: aws.String(vpcID),
 		EnableDnsSupport: &ec2Types.AttributeBooleanValue{
 			Value: aws.Bool(true),
 		},
+	})
+	if err != nil {
+		return "", nil, errors.Wrapf(err, "failed to enable DNS support for VPC %s", vpcID)
+	}
+	_, err = client.ModifyVpcAttribute(context.TODO(), &ec2.ModifyVpcAttributeInput{
+		VpcId: aws.String(vpcID),
 		EnableDnsHostnames: &ec2Types.AttributeBooleanValue{
 			Value: aws.Bool(true),
 		},
@@ -141,17 +149,41 @@ func createVPC(ctx context.Context, client *ec2.Client, region string) (string, 
 		return "", nil, errors.Wrapf(err, "failed to enable DNS support for VPC %s", vpcID)
 	}
 
-	subnetOutput, err := client.CreateSubnet(context.TODO(), &ec2.CreateSubnetInput{
+	availabilityZonesOutput, err := client.DescribeAvailabilityZones(ctx, &ec2.DescribeAvailabilityZonesInput{})
+	if err != nil {
+		return "", nil, errors.Wrap(err, "failed to describe availability zones")
+	}
+	var subnetAvZones []string
+	for _, az := range availabilityZonesOutput.AvailabilityZones {
+		if az.State == ec2Types.AvailabilityZoneStateAvailable && len(subnetAvZones) < 2 {
+			subnetAvZones = append(subnetAvZones, *az.ZoneName)
+		}
+	}
+	if len(subnetAvZones) < 2 {
+		return "", nil, errors.Wrapf(err, "there is no sufficient availability zones available in region %s", region)
+	}
+
+	subnet1Output, err := client.CreateSubnet(ctx, &ec2.CreateSubnetInput{
 		VpcId:            aws.String(vpcID),
 		CidrBlock:        aws.String("10.0.1.0/24"),
-		AvailabilityZone: aws.String(region),
+		AvailabilityZone: aws.String(subnetAvZones[0]),
+	})
+	if err != nil {
+		return "", nil, errors.Wrapf(err, "failed to create subnet within the VPC %s", vpcID)
+	}
+
+	subnet2Output, err := client.CreateSubnet(ctx, &ec2.CreateSubnetInput{
+		VpcId:            aws.String(vpcID),
+		CidrBlock:        aws.String("10.0.2.0/24"),
+		AvailabilityZone: aws.String(subnetAvZones[1]),
 	})
 	if err != nil {
 		return "", nil, errors.Wrapf(err, "failed to create subnet within the VPC %s", vpcID)
 	}
 
 	subnetIDs := []string{
-		*subnetOutput.Subnet.SubnetId,
+		*subnet1Output.Subnet.SubnetId,
+		*subnet2Output.Subnet.SubnetId,
 	}
 	return vpcID, subnetIDs, nil
 }
